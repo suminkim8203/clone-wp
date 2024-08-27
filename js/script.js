967b710629c830e0558886fa3d440dd7daf17bfd
suminkim8203 <kimsm8203@gmail.com>
2024-08-27 17:25:21 +0900

    docs : vercel 배포

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 window.onload = function () {
+  // 모달창 기능
+  let modalWrap = document.querySelector(".modal-wrap");
+  let modalClose = document.querySelector(".modal-close");
+  modalClose.addEventListener("click", function () {
+    modalWrap.classList.add("fadeOut");
+    modalWrap.addEventListener("animationend", () => {
+      modalWrap.style.display = "none";
+    });
+  });
   // 위로 가기
   const goTopBt = document.querySelector(".gotop");
   goTopBt.addEventListener("click", function () {
